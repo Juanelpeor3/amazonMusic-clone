02eb06b4129b41845f9deb52fc9648c1bbace0d7
Juanelpeor3 <157135100+Juanelpeor3@users.noreply.github.com>
2024-05-03 00:29:50 +0200

    Funciones y canciones
    Cambiadas las funciones y las funcionalidades de las canciones, ahora solo se reproducira una a la vez.
    
    Añadido EasterEGG en el login
    Añadido nueva positicion en el loader del login, este no aparecia centrado dependiendo de las dimensiones de la pantalla

--- ProyectoMusicaStreaming/js/cancion.js
+++ ProyectoMusicaStreaming/js/cancion.js
@@ -1,20 +1,67 @@
+let audio = null;
+
 function musica(nombre) {
+  //constantes que utilizaremos en las funciones
   const despausaButton = document.getElementById("despausa");
   const pausaButton = document.getElementById("pausa");
   const volumenSlider = document.getElementById("volumen");
   const duracionSlider = document.getElementById("duracion");
+  // Si hay un audio reproduciendose lo pausamos
+  if (audio) {
+    audio.pause();
+  }
   if (nombre == "p1") {
-    let audio = new Audio("music/Maestro_Sin_Igual.mp3");
+    audio = new Audio("music/Maestro_Sin_Igual.mp3");
+    audio.play();
+
+    //duracion
+    sliderDuracion(audio);
+    //logica pausa y despausa
+    pausaDespausa(audio);
+    //ajustar volumen
+    ajustarVolumen(audio);
+  }
+  if (nombre == "p2") {
+    audio = new Audio("music/Salon_Vacio.mp3");
     audio.play();
     //duracion
+    sliderDuracion(audio);
+    //logica pausa y despausa
+    pausaDespausa(audio);
+    //ajustar volumen
+    ajustarVolumen(audio);
+  }
+
+  if (nombre == "p3") {
+    audio = new Audio("music/Es_tres.mp3");
+    audio.play();
+    //duracion
+    sliderDuracion(audio);
+    //logica pausa y despausa
+    pausaDespausa(audio);
+    //ajustar volumen
+    ajustarVolumen(audio);
+  }
+  function sliderDuracion(audio) {
     audio.addEventListener("loadedmetadata", function () {
       var duracion = audio.duration;
       duracionSlider.max = duracion;
       //actualizar el slider con la duracion del audio
+      //asocio los minutos y segundos totales del audio a una varible para
+      //asignarles en el caso de que lo necesiten un 0 a la izquierda
+      var minutosTotal = Math.floor(duracion / 60);
+      var segundosTotal = Math.floor(duracion % 60);
+      if (minutosTotal < 10) {
+        minutosTotal = "0" + minutosTotal;
+      }
+      if (segundosTotal < 10) {
+        segundosTotal = "0" + segundosTotal;
+      }
+      //Actualizo el id duration-total con el siguiente contenido
       document.getElementById("duration-total").innerHTML =
-        Math.floor(duracion / 60) + ":" + Math.floor(duracion % 60);
+        minutosTotal + ":" + segundosTotal;
       duracionSlider.value = duracion;
-      //Muevo yo el slider y la cancion va a ese minuto
+      //Muevo yo el slider y la cancion va a ese minuto y segundo
       duracionSlider.addEventListener("input", function () {
         var nuevaDuracion = Math.min(duracion, duracionSlider.value);
         duracionSlider.value = nuevaDuracion;
@@ -24,13 +71,26 @@ function musica(nombre) {
       //mueve el slider segun la duracion de la cancion
       audio.addEventListener("timeupdate", function () {
         const currentTime = audio.currentTime;
+        //asocio los minutos y segundos actuales del audio a una varible para
+        //asignarles en el caso de que lo necesiten un 0 a la izquierda
+        var minutosActual = Math.floor(currentTime / 60);
+        var segundosActual = Math.floor(currentTime % 60);
+        if (minutosActual < 10) {
+          minutosActual = "0" + minutosActual;
+        }
+        if (segundosActual < 10) {
+          segundosActual = "0" + segundosActual;
+        }
+        //Actualizo el id duration con el siguiente contenido
         document.getElementById("duration").innerHTML =
-          Math.floor(currentTime / 60) + ":" + Math.floor(currentTime % 60);
+          minutosActual + ":" + segundosActual;
+        //Actualizo el slider
         const progress = currentTime / audio.duration;
         duracionSlider.value = progress * duracionSlider.max;
       });
     });
-    //logica pausa y despausa
+  }
+  function pausaDespausa(audio) {
     despausaButton.style.display = "none";
     pausaButton.style.display = "block";
     despausaButton.addEventListener("click", function () {
@@ -38,42 +98,9 @@ function musica(nombre) {
     });
     pausaButton.addEventListener("click", function () {
       audio.pause();
-    });
-    //ajustar volumen
-    volumenSlider.addEventListener("input", function () {
-      var a = volumenSlider.value;
-      var a = a / 100;
-      audio.volume = a;
     });
   }
-  if (nombre == "p2") {
-    let audio = new Audio("music/Salon_Vacio.mp3");
-    audio.play();
-    despausaButton.style.display = "none";
-    pausaButton.style.display = "block";
-    despausaButton.addEventListener("click", function () {
-      audio.play();
-    });
-    pausaButton.addEventListener("click", function () {
-      audio.pause();
-    });
-    volumenSlider.addEventListener("input", function () {
-      var a = volumenSlider.value;
-      var a = a / 100;
-      audio.volume = a;
-    });
-  }
-  if (nombre == "p3") {
-    let audio = new Audio("music/Es_tres.mp3");
-    audio.play();
-    despausaButton.style.display = "none";
-    pausaButton.style.display = "block";
-    despausaButton.addEventListener("click", function () {
-      audio.play();
-    });
-    pausaButton.addEventListener("click", function () {
-      audio.pause();
-    });
+  function ajustarVolumen(audio) {
     volumenSlider.addEventListener("input", function () {
       var a = volumenSlider.value;
       var a = a / 100;
