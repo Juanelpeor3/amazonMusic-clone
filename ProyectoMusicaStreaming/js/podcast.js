43875fb09c976f787d2de8c84a4e5a6d1b5bd0ae
Juanelpeor3 <157135100+Juanelpeor3@users.noreply.github.com>
2024-05-07 01:06:16 +0200

    Delete ProyectoMusicaStreaming directory

--- ProyectoMusicaStreaming/js/podcast.js
+++
@@ -1,7 +0,0 @@
-let urlindex = new URL(window.location.href);
-let username = urlindex.searchParams.get("user");
-let radiobutton = document.getElementById("podcasts");
-
-radiobutton.addEventListener("click", function () {
-  window.location.href = "podcast.html?" + "user=" + username;
-});
