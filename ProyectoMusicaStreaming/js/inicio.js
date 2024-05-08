124952d39bd477662b52d1b0969fb742d8d55335
Juanelpeor3 <157135100+Juanelpeor3@users.noreply.github.com>
2024-05-08 02:10:18 +0200

    Delete ProyectoMusicaStreaming directory

--- ProyectoMusicaStreaming/js/inicio.js
+++
@@ -1,7 +0,0 @@
-let urlradio = new URL(window.location.href);
-let username = urlradio.searchParams.get("user");
-let homebutton = document.getElementById("inicio");
-
-homebutton.addEventListener("click", function () {
-  window.location.href = "index.html?" + "user=" + username;
-});
--- ProyectoMusicaStreaming/js/volver.js
+++
@@ -1,9 +0,0 @@
-let urlIndex = new URL(window.location.href);
-let paramIndex = urlIndex.searchParams.get("user");
-if (
-  paramIndex == null ||
-  paramIndex == "" ||
-  (paramIndex !== "user" && paramIndex !== "pepe" && paramIndex !== "ibai")
-) {
-  window.location.href = "login.html";
-}
